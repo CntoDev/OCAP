e3f01df44f5db9dd1f8009ffb81aaf6165e9030b
Ivan Šakić <ivan.sakic3@gmail.com>
2017-04-27 22:13:41 +0200

    Further renaming

---
+++ dist/userconfig/cntr/config.hpp
@@ -0,0 +1,8 @@
+cntr_captureInterval = 1;
+cntr_minPlayerCount = 1;
+cntr_endCaptureOnEndMission = false;
+cntr_endCaptureOnNoPlayers = true;
+
+cntr_exportPath = "C:\wamp64\www\cntr\data";
+
+cntr_debug = true;
